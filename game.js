0334a0b13cb70a9ead1f9c1f0a0cc9ee872a98e7
Vibhushit Tyagi <vibhushit10@gmail.com>
2020-07-22 21:50:28 +0530

    Draw and Update

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 1;
+
 
 function main(currentTime){
 	window.requestAnimationFrame(main);
@@ -8,6 +10,17 @@ function main(currentTime){
 
 	console.log('Render');
 	lastRenderTime = currentTime;
+
+	update();
+	draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update(){
+	updateSnake();
+}
+
+function draw(){
+	drawSnake();
+}
---
+++ snake.js
@@ -0,0 +1,9 @@
+export const SNAKE_SPEED = 1;
+
+export function update(){
+	console.log('update snake');
+}
+
+export function draw(){
+	console.log('draw snake');
+}
